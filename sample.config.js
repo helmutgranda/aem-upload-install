b92dd670ce70998f27e6e3d3ec376523f74af08d
Helmut Granda <contact@helmutgranda.com>
2017-09-06 11:25:57 -0500

    Sample configuration file to be used as a guide.

---
+++ sample.config.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.SERVER_ROOT = '//IT/.../aem';
+config.APP_ROOT = '/Users/.../aem/';
+config.APP_SERVER = 'http://localhost:4502/crx/packmgr/service.jsp';
+config.LOCATIONS = [
+    'content.zip',
+    'nilm-integration-content.zip',
+    'overlays.zip'
+];
+
+
+module.exports = config;
